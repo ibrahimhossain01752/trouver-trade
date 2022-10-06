66a3f6b9dffdb240a55fd8471b5acd86f8844fde
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2022-10-06 17:09:14 +0600

    banner pera added

--- src/component/Home/Banner.js
+++ src/component/Home/Banner.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import './Banner.css';
 
 const Banner = () => {
     return (
-        <div class="card-group">
+        <div class="container">
+            <div class="card-group">
   <div class="card">
-    <img src="..." class="card-img-top" alt="..."/>
+    {/* <img src="..." class="card-img-top" alt="..."/> */}
+    <h3 class="card-title banner_one">GANGA CONFECTIONERIES PVT.LTD.</h3>
+    {/* <button class="arrow">REWARD RS.500</button> */}
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      {/* <h3 class="card-title">GANGA CONFECTIONERIES PVT.LTD.</h3> */}
+      <img src="https://cdn.dnaindia.com/sites/default/files/styles/full/public/2020/09/25/927447-sweets-new.jpg" class="card-img-top" alt="..."/>
+      <button class="arrow">REWARD RS.500</button>
+      <p class="card-text">Ganga Confectioneries is listed under Sweet Shops in Rajendra Nagar Sector 5, Sahibabad Delhi. Check Address, Contact Number, Rating & Reviews, Photos, Maps etc, on Justdial.<span class="banner-para">Cakes are pretty much the highlight of all events in our lives. Any celebration, whether a birthday, an aniversary, or an acoomplishment, is incomplete without a delicious cake. A cake can be defined as an defined as an ideal sweet dish when it just melts in your mouth and has the most distinctive flavour with the perfect filling and texture. All your sweet carvings can be satisfied at the cake shops in Sahibabad, Delhi, where you would never be disappointed with the availability of a variety of scrumptious cakes and bankery items.</span></p>
+     
     </div>
     <div class="card-footer">
       <small class="text-muted">Last updated 3 mins ago</small>
@@ -25,6 +32,7 @@ const Banner = () => {
     </div>
   </div>
 </div>
+        </div>
     );
 };
 
